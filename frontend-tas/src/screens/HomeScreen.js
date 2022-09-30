b9112d54f7037e1e9ba4fc0e38da9e10cd35b331
PYK <you@example.com>
2022-09-30 12:11:26 +0530

    connected frontend with basic backend and concurrently running the server

--- frontend-tas/src/screens/HomeScreen.js
+++ frontend-tas/src/screens/HomeScreen.js
@@ -1,9 +1,23 @@
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import axios from 'axios'
 
 const Homescreen = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+
+        const fetchProducts = async () => {
+            const { data } = await axios.get('/api/products')
+
+            setProducts(data);
+        }
+
+        fetchProducts()
+    }, [])
+
     return (
         <>
             <h1>Latest Products</h1>
